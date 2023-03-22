9812559a916118457032c476b6be1f56121ace4a
afratetlay <afratetlay@gmail.com>
2023-03-22 15:46:13 +0100

    adding the expenseitem file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th</div>
+      <div>
+        <h2>Car Insuarnce</h2>
+        <div>£269.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
